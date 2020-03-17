cadbeb32ece04787451991179a19993c2acbdef8
Vyraal1 <kevin.afable@gmail.com>
2020-03-17 14:23:02 -0400

    feat: word challenge

--- word_challenge/words.py
+++ word_challenge/words.py
@@ -1,7 +1,33 @@
 from util import Queue
 f = open('words.txt', 'r')
 words = f.read().lower().split("\n")
+f.close()
+
 # Step 2: Build the Graph
+# Create an equality functions
+
+
+def words_are_neighbors(w1, w2):
+    ''' Return boolean whether or not words are one letter apart '''
+    list_word = list(w1)
+    for i in range(len(list_word)):
+        for letter in 'abcdefghijklmnopqrstuvwyz':
+            temp_word = list_word.copy()
+            temp_word[i] = letter
+            if "".join(temp_word) == w2:
+                return True
+    return False
+
+
+def words_are_neighbors2(word1, word2):
+    for i in range(len(word1)):
+        list_word1 = list(word1)
+        list_word2 = list(word2)
+        list_word1.pop(i)
+        list_word2.pop(i)
+        if list_word1 == list_word2:
+            return True
+    return False
 
 # Step 3: Traverse the Graph
 
