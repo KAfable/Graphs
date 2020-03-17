104e72d75e13e8f740f94d79cc7aa59be6b9d406
Vyraal1 <kevin.afable@gmail.com>
2020-03-17 13:10:36 -0400

    feat: graph traversal

---
+++ word_challenge/words.py
@@ -0,0 +1,23 @@
+from util import Queue
+
+# Step 2: Build the Graph
+
+# Step 3: Traverse the Graph
+
+
+def word_ladder(self, begin_word, end_word):
+    '''
+    Return a list of words containing the least amount of changes from begin_word to end_word.
+    '''
+
+    q = Queue()
+    q.enqueue([begin_word])
+    visited = set()
+    while q.size() > 0:
+        path = q.dequeue()
+        word = path[-1]
+        if word == end_word:
+            return path
+        if word not in visited:
+            for neighbor in get_neighbors():
+                q.enqueue([*path, neighbor])
