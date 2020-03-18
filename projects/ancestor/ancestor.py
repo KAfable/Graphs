7ce4e8eab673ce21913980c0436c30997248a4d5
Kevin <kevin.afable@gmail.com>
2020-03-18 10:22:15 -0400

    chore: renaming neighbor function

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -7,5 +7,5 @@ def earliest_ancestor(ancestors, starting_node):
 
     # how the hell do you get ancestors?
     # filter through ancestors for the tuple
-    def get_ancestors(ancestors, starting_node):
+    def get_parents(ancestors, starting_node):
         return filter(lambda person: person[1] == starting_node, ancestors)
